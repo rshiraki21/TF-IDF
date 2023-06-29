31ee1f340af7403676d935d576c69a765ae7afb2
rshiraki21 <rshiraki03@gmail.com>
2023-06-28 15:38:20 -1000

    feature names need more processing

--- tfidf.py
+++ tfidf.py
@@ -6,46 +6,36 @@ import re
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# path_to_json_files = "Internship\Internship Data ArmyAPI Pull_06222023"
-path_to_json_files = "Internship\TestJSON"
+stopwords = nltk.corpus.stopwords.words("english")
 
-# stopwords
-stopwords = nltk.corpus.stopwords.words('english')
+path_to_json = "Internship\Internship Data ArmyAPI Pull_06222023"
+# path_to_json = "Internship\TestJSON"
 
-def process_text(unprocessed_text):
-    unprocessed_text = "".join([word.lower() for word in unprocessed_text if word not in string.punctuation]) # removes punctuation
-    tokens = re.split('\W+', unprocessed_text) # splits text into a list of words
-    unprocessed_text = [word for word in tokens if word not in stopwords] # removes stopwords ()
-    for word in unprocessed_text:
-        if word.isnumeric():
-            word = "<NUM>"
-    return unprocessed_text
+# * Function to process text
+def clean_text(text):
+    text = "".join([word.lower() for word in text if word not in string.punctuation]) # remove punctuation and makes all text lowercase
+    tokens = re.split('\W+', text) # causes text to split when it sees more than one non-word characters (whitespace, special characters, etc.)
+    text = [word for word in tokens if word not in stopwords] # remove stopwords
+    for k in range(len(text)): # tokenizes numbers
+        if text[k].isnumeric(): 
+            text[k] = "<NUM>"
+    return text
 
-# * List of all the text values from json files
-corpus = []
-for file in os.listdir(path_to_json_files):
-    filename = "%s/%s" % (path_to_json_files, file)
-    with open(filename, "r") as f:
-        article_json = json.load(f)
-        corpus.append(process_text(article_json["text"]))
-print(corpus)
+# * Creates a list consisting of all the text for processing
+unprocessed_texts = []
+for file in os.listdir(path_to_json):
+    filename = "%s/%s" % (path_to_json, file)
+    with open (filename, "r") as f:
+        article = json.load(f)
+        unprocessed_texts.append(article["text"])
 
-vectorizer = TfidfVectorizer(stop_words="english", encoding="utf-8") # initialize tfidf
+# * Initializing TF-IDF
+vectorizer = TfidfVectorizer(analyzer=clean_text)
+tfidf = vectorizer.fit_transform(unprocessed_texts)
 
-tfidf = vectorizer.fit_transform(corpus) # apply tfidf to corpus
-
-print("Tokens used as features are : ")
-print(vectorizer.get_feature_names_out())
-
-print("\n Size of array. Each row represents a document. Each column represents a feature/token")
-print(tfidf.shape)
-
-print("\n Actual TF-IDF array")
-print(tfidf.toarray())
-
-for ele1, ele2 in zip(vectorizer.get_feature_names_out(), vectorizer.idf_): # print idf values for common words
-    print(ele1, ':', ele2)
-
-pd.DataFrame(tfidf.toarray()).to_csv("Internship/ArticleTFIDF") # export tfidf matrix to a csv
-
+# * Exporting findings to csv
 pd.DataFrame(vectorizer.get_feature_names_out()).to_csv("Internship/feature_names") # export feature names to a csv
+pd.DataFrame(tfidf.toarray()).to_csv("Internship/tfidf_matrix") # export tfidf matrix to a csv
+
+
+
