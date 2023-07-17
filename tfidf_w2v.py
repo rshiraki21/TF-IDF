d6d549e1a07ac4ece438133ffbf30184f8a877b0
rshiraki21 <rshiraki03@gmail.com>
2023-07-17 12:30:06 -1000

    error w/ list dimensions

--- tfidf_w2v.py
+++ tfidf_w2v.py
@@ -1,5 +1,7 @@
+import json
 import gensim.downloader as pretrained_models
 import nltk
+import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from tfidf import clean_text, generate_corpus, partition_matrix, sort_partitions
 
@@ -8,12 +10,15 @@ path_to_json = "Internship Data ArmyAPI Pull_06222023"
 
 google_embeddings = pretrained_models.load(name="word2vec-google-news-300")
 
-corpus = generate_corpus(folder_path=path_to_json)
-
-vectorizer = TfidfVectorizer(analyzer=clean_text) # applies clean_text() as an analyzer
-tfidf = vectorizer.fit_transform(corpus)
-tfidf_array = tfidf.toarray()
-tfidf_words = vectorizer.get_feature_names_out()
+# * Creates a list consisting of all the processed text
+def generate_clean_corpus(folder_path):
+    corpus = []
+    for file in os.listdir(folder_path):
+        filename = "%s/%s" % (folder_path, file)
+        with open (filename, "r") as f:
+            article = json.load(f)
+            corpus.append(clean_text(article["text"]))
+    return corpus
 
 def generate_phrase_embeddings(embeddings):
     phrase_embeddings = []
@@ -29,9 +34,16 @@ def generate_phrase_embeddings(embeddings):
 def generate_combined_embeddings(phrase_embeddings, tfidf_matrix):
     combined_embeddings = []
     for i, embedding in enumerate(phrase_embeddings):
-        combined_embedding = embedding * tfidf_matrix[i].flatten() # ! operands could not be broadcast together with shapes (300,) (7418,)
+        combined_embedding = embedding * tfidf_matrix[i].flatten() # ! operands could not be broadcast together with shapes (300,) (654,)
         combined_embeddings.append(combined_embedding)
     return combined_embeddings
+
+corpus = generate_clean_corpus(folder_path=path_to_json)
+
+vectorizer = TfidfVectorizer(analyzer=clean_text) # applies clean_text() as an analyzer
+tfidf = vectorizer.fit_transform(corpus)
+tfidf_array = tfidf.toarray()
+tfidf_words = vectorizer.get_feature_names_out()
 
 phrase_embeddings = generate_phrase_embeddings(embeddings=google_embeddings)
 combined_embeddings = generate_combined_embeddings(phrase_embeddings=phrase_embeddings, tfidf_matrix=tfidf_array)
